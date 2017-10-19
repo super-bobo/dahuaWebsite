056d29d1d56d2a616fd540a12dcfda58d6d7440a
wang_hongbo <286892469@qq.com>
2017-10-19 14:32:36 +0800

    :Runner: delete

--- src/main.js
+++ src/main.js
@@ -1,26 +1,4 @@
 
-// let u = navigator.userAgent;
-
-// if (u.indexOf('Trident') > -1) {
-// 	window.location.href = "http://www.dahuasecurity.com";
-// }
-// console.log(u)
-// if (u.toLowerCase().indexOf("safari") > -1 && u.toLowerCase().indexOf("coolpad") > -1) {
-// 	var ver=u.toLowerCase().match(/safari\/([\d.]+)/)[1];
-//     console.log(ver)
-//     if(ver.split('.')[0] < 537){
-//     	window.location.href = "http://www.dahuasecurity.com";
-//     }
-// }
-
-// if (u.toLowerCase().indexOf("chrome") > -1 && u.toLowerCase().indexOf("coolpad") > -1) {
-// 	var ver=u.toLowerCase().match(/chrome\/([\d.]+)/)[1];
-//     console.log(ver)
-//     if(ver.split('.')[0] < 30){
-//     	window.location.href = "http://www.dahuasecurity.com";
-//     }
-// }
-
 import 'babel-polyfill' //兼容老版本
 
 import Vue from 'vue'
